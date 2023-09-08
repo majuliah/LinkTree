8f42df44e89055ceacf11e73c73d16e3fa97d12f
majulha <majulialcarvalho@gmail.com>
2023-09-08 18:30:15 -0300

    implementando funcionalidades finais

---
+++ projetoHtml/index.js
@@ -0,0 +1,12 @@
+function toggleMode(){
+    const html = document.body;
+    html.classList.toggle('light')
+
+    const img = document.querySelector("#profile img")
+    
+    if(html.classList.contains('light')){
+        img.setAttribute('src', './assets/avatar-day.png')
+    }else{
+        img.setAttribute('src', './assets/avatar-night.png')
+    }
+}
