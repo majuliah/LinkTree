71f15ecdb1e03a520c19beb203813785e9142564
majulha <majulialcarvalho@gmail.com>
2023-10-03 16:25:24 -0300

    enviando nova imagem

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function toggleMode(){
     const img = document.querySelector("#profile img")
     
     if(html.classList.contains('light')){
-        img.setAttribute('src', './assets/avatar-day.png')
+        img.setAttribute('src', 'assets/avatar-day.png')
     }else{
-        img.setAttribute('src', './assets/avatar-night.png')
+        img.setAttribute('src', 'assets/avatar-night.png')
     }
 }
